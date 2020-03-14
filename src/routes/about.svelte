c3a87d2adbff7f2bc8b8c08355b4ebec29915085
Wowbcf <brendenferguson@gmail.com>
2020-03-14 15:12:33 -0700

    init

---
+++ src/routes/about.svelte
@@ -0,0 +1,24 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About</h1>
+
+<p>My name is Brenden Ferguson.</p>
+
+<p>
+  I've been making simple websites for the last fifteen years for fun.
+  <br />
+  I got my start with geocities, then worked with Dreamweaver, before finally
+  settling on plain HTML with javascript snippets I found on the web.
+</p>
+
+<p>
+  Last year, I decided to introduce myself to real coding by taking a python
+  course to bring more value to my employer and loved it.
+  <br />
+  I decided to dive in and become a programmer.
+  <br />
+  I've always loved the internet and building things, so this became the natural
+  place to bring my newly forged programming ability.
+</p>
---
+++ src/routes/index.svelte
@@ -0,0 +1,61 @@
+<style>
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  figure {
+    margin: 0 0 1em 0;
+  }
+
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Brenden Ferguson</title>
+</svelte:head>
+<p>
+  <strong>
+    <h1>Hello! I'm Brenden.</h1>
+    <h3>
+      I've been making websites for 15 years for fun.
+      <br />
+      Recently I decided to do it professionally.
+    </h3>
+  </strong>
+</p>
+
+<p>
+  <a href="https://github.com/fergusonB" target="_blank">
+    <img style="width:64px" src="./assets/gh.png" alt="GitHub" />
+  </a>
+  <a href="https://www.linkedin.com/in/brendenferguson/" target="_blank">
+    <img style="width:64px" src="./assets/li.png" alt="LinkedIn" />
+  </a>
+</p>
+
+<figure>
+  <img alt="my face" src="./assets/face.jpg" />
+</figure>
