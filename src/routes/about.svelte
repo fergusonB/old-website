76da35c79e1b3bb05efca37cbe6369044db7ab39
Wowbcf <brendenferguson@gmail.com>
2020-03-16 07:34:54 -0700

    about updates

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -22,3 +22,25 @@
   I've always loved the internet and building things, so this became the natural
   place to bring my newly forged programming ability.
 </p>
+
+<h2>What do I like right now?</h2>
+<p>
+  This website is statically generated with Svelte and Sapper, hosted on GitHub,
+  and is protected by Cloudflare.
+  <br />
+  The entire website including images (at the time of writing) is under 100kB,
+  has a perfect Lighthouse score, and also functions as a progressive web app
+  (PWA).
+  <br />
+  <br />
+  <img
+    style="display:block;margin-left:auto;margin-right:auto;"
+    src="./assets/about/lighthouse.png"
+    alt="lighthouse" />
+
+</p>
+<p>
+  My favorite language right now is python. It started this journey and I find
+  it incredibly intuitive. Comprehensions make me happy, and python is so easy
+  to read.
+</p>
