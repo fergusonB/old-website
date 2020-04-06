1ee868dfac305346d538f72fd07edb13503baf53
Brenden Ferguson <brendenferguson@gmail.com>
2020-04-05 17:29:07 -0700

    early cov model

---
+++ src/routes/projects/cov.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from 'svelte';
+
+
+    let summary;
+ 
+
+
+
+	onMount(async () => {
+		const res = await fetch(`https://api.covid19api.com/summary`);
+        summary = await res.json();
+
+        
+    });
+    
+
+    
+
+</script>
+
+
+{#if summary}
+     {#each summary.Countries as country}
+     {JSON.stringify(country.Country)}
+     <br>
+{/each}
+{:else}
+     loading
+{/if}
