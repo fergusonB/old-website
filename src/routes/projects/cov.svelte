adfb829bab29620019a19768727697b2feb1aaa7
Brenden Ferguson <brendenferguson@gmail.com>
2020-04-05 18:15:05 -0700

    before making changes to logic

--- src/routes/projects/cov.svelte
+++ src/routes/projects/cov.svelte
@@ -1,11 +1,14 @@
 <script>
-//initial page render
+//initial page render, show summary data - total cases, new cases ( %) , deaths, new deaths %, recovered, new recovered %
+//selection for countries, top countries beginning?
 
 
     import { onMount } from 'svelte';
 
 
     let summary;
+    let state = ''
+    let countrydata
  
 
 
@@ -17,17 +20,44 @@
         
     });
     
+    const currentCountry = async (state) =>{
+		const res = await fetch(`https://api.covid19api.com/${state}/status/confirmed`);
+        countrydata = await res.json();
+    }
 
     
 
 </script>
 
+<h1>Current COVID19 Data</h1>
 
-{#if summary}
-     {#each summary.Countries as country}
-     {JSON.stringify(country.Country)}
+
+
+<p>Select a Country</p>
+<button on:click={()=>{state=`Algeria`}}>state test</button>
+{#if summary && state ===''}
+
+<h2>Global</h2>
+<p>
+     Total Cases: {JSON.stringify(summary.Global.TotalConfirmed)}
      <br>
-{/each}
+          New Cases: {JSON.stringify(summary.Global.NewConfirmed)} (Change of +{((summary.Global.NewConfirmed/summary.Global.TotalConfirmed)*100).toFixed(2)}%)
+     <br>
+Total Deaths: {JSON.stringify(summary.Global.TotalDeaths)}
+<br>
+New Deaths: {JSON.stringify(summary.Global.NewDeaths)} (Change of +{((summary.Global.NewDeaths/summary.Global.TotalDeaths)*100).toFixed(2)}%)
+     <br>
+Total Recovered: {JSON.stringify(summary.Global.TotalRecovered)}
+<br>
+New Recoveries: {JSON.stringify(summary.Global.NewRecovered)} (Change of +{((summary.Global.NewRecovered/summary.Global.TotalRecovered)*100).toFixed(2)}%)
+</p>
+
+
+
+
+{:else if  state !== ``}
+
+
 {:else}
-     loading
+     Getting data from covid19api.com ...
 {/if}
