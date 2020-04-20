ba3eae09af67068550e876b52a31ce2ceedc4e54
fergusonB <brendenferguson@gmail.com>
2020-04-19 21:20:42 -0700

    localstorage

---
+++ src/routes/projects/localstorage.svelte
@@ -0,0 +1,23 @@
+<script>
+
+
+if(process.browser){
+    if (!localStorage.gold){
+        localStorage.gold = 0
+        
+    }
+}
+
+let gold = localStorage.gold
+
+</script>
+
+
+
+{#if process.browser}
+    {gold}
+     <button  on:click={()=>{localStorage.gold = Number(localStorage.gold)+5000}} on:click={()=>gold= localStorage.gold}>hit</button>
+{:else}
+     Loading...
+{/if}
+
