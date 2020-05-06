2bbe0bf1c921ea4d637c2e48f08c1766b6ce8124
Wowbcf <brendenferguson@gmail.com>
2020-05-06 14:27:49 -0700

    observer foods

---
+++ src/components/observer/Food.svelte
@@ -0,0 +1,63 @@
+<script>
+
+class Food {
+  constructor(quantity, item, expires, price) {
+    this.Quantity = quantity;
+    this.Item = item;
+    this.Expires = expires;
+    this.Price = price
+  }
+}
+
+
+// master list
+let foods = [];
+let id = []
+
+$: jsonOutput = JSON.stringify(foods)
+
+// example
+let inputQty = 1
+let inputItem = 'Item'
+let inputDate = '2100-01-01'
+let inputPrice = 0.00
+
+// add food
+const foodFunction = () =>{
+    foods.push(new Food(inputQty,inputItem,inputDate,inputPrice))
+    foods = foods
+}
+
+
+</script>
+
+<h2 style="text-align:center;">Food</h2>
+
+{#each foods as food,i}
+     <div class="card">
+    {#each Object.keys(food) as key}
+        {key}: {food[key]}
+        <br>
+     {/each}
+     <button on:click={()=>{
+         foods.splice(i,1)
+         foods = foods
+    }}>Delete</button>
+
+</div>
+
+{/each}
+<div class="card">
+<h3>New</h3>
+Quantity: <input type="number" bind:value={inputQty}>
+<br>
+Item: <input type="text" on:click={()=>inputItem=''} bind:value={inputItem}>
+<br>
+Expiration: <input type="date" bind:value={inputDate}>
+<br>
+Price: <input type="number" bind:value={inputPrice}>
+<br>
+<button on:click={foodFunction}>Add</button>
+</div>
+
+{jsonOutput}
---
+++ src/routes/projects/observer.svelte
@@ -0,0 +1,32 @@
+<script>
+import Task from "../../components/observer/Task.svelte";
+import Food from "../../components/observer/Food.svelte";
+</script>
+
+<svelte:head>
+  <title>The Observer</title>
+</svelte:head>
+
+<style>
+.container{
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+}
+</style>
+
+<h1>The Observer</h1>
+
+
+<p>
+    The Observer uses local storage, it does not record any of your data. The data is remembered only on this device; however, you may download and load the JSON to new devices.
+</p>
+
+
+<div class="container">
+
+<div ><Task/></div>
+
+<div><Food/></div>
+
+</div>
+
