259809c62aa5345eb03a78067d552ef97409a3d6
Wowbcf <brendenferguson@gmail.com>
2020-05-11 15:18:09 -0700

    trip date and trip location

--- src/components/observer/FoodTrip.svelte
+++ src/components/observer/FoodTrip.svelte
@@ -1,12 +1,18 @@
 <script>
 export let data;
 export let currentTrip;
+let selectedTrip;
 
 $: trips = [...new Set(data.map(x=>x.TripID).filter(x=>x!==currentTrip))];
 $: tripTotal = process.browser ? data.map(x=> x.TripID === selectedTrip ? x.Price : 0).reduce((a,b)=>a+b,0).toFixed(2) : 'Waiting...'
 $: tripAverage = process.browser ? Number(tripTotal) / (data.filter(x=>x.TripID===selectedTrip,0).length) : 'Waiting...'
+$: tripDate = process.browser  ? data.map(x=> x.TripID === selectedTrip ? x.Date : 0).filter(x=>x!==0)[0]: 'Waiting...'
+$: tripLocation = process.browser  ? data.map(x=> x.TripID === selectedTrip ? x.Location : 0).filter(x=>x!==0)[0]: 'Waiting...'
 
-let selectedTrip;
+
+
+
+
 
 </script>
 
@@ -29,9 +35,9 @@ Selected (showing data for selected):
 </p>
 
 <p>
-Date: 
+Date: {tripDate || `Waiting...`}
 <br>
-Location: 
+Location: {tripLocation || `Waiting...`}
 <br>
 Trip Total: ${tripTotal}
 <br>
