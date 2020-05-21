3291dc95e63ee6f501742c3867bf688178a61fb7
Brenden Ferguson <brendenferguson@gmail.com>
2020-05-20 17:05:50 -0700

    starting work on binomial

--- src/components/statistics/Binomial.svelte
+++ src/components/statistics/Binomial.svelte
@@ -1,1 +1,35 @@
 <h2>Binomial</h2>
+
+<script>
+
+
+
+let x = 1 //number of successes
+let n = 10 // number of trials
+
+let p = .5 // probability of success
+
+$: q =1-p // probability of failure
+
+const fact = (n)=> n ? n*fact(n-1):1
+
+
+let answer
+$: if (x&&n&&p&&q){
+    answer = probabilityMass(x,n,p,q)
+}
+
+const probabilityMass = (x,n,p,q) =>{
+    return (fact(n) /    fact(x) * fact(( n-x))) * (p**x)  * (q**((n-x)))
+}
+
+</script>
+
+<input bind:value={x} type="number"> x, number of successes
+<br>
+<input bind:value={n} type="number"> n, number of trials
+<br>
+<input bind:value={p} type="number"> p, probability of success (decimal)
+<br>
+
+{answer}
