70d70625fd06bccb4dfd7a681807c1461c912aa8
Brenden Ferguson <brendenferguson@gmail.com>
2020-05-21 09:31:03 -0700

    prob format

--- src/components/statistics/Probability.svelte
+++ src/components/statistics/Probability.svelte
@@ -1,44 +1,37 @@
-<h2>Probability</h2>
-
 <script>
-
-let favorable = 1
-let total = 4
-let attempts = 1
-
+  let favorable = 1;
+  let total = 4;
+  let attempts = 1;
 </script>
 
 <style>
-#main{
-    display:grid;
+  #main {
+    display: grid;
     grid-template-columns: 250px 1fr;
-}   
-#main input{
-    text-align:center;
-}
+  }
+  #main input {
+    text-align: center;
+  }
 </style>
 
+<h2>Probability</h2>
 <div id="main">
-<p >
+  <p>
+    Favorable outcomes:
+    <br />
+    Total outcomes:
+    <br />
+    Trials:
+    <br />
+    Probability of success on every trial: {((favorable / total) ** attempts * 100).toFixed(4)}%
+  </p>
+  <p>
+    <input bind:value={favorable} type="number" />
+    <br />
+    <input bind:value={total} type="number" />
+    <br />
+    <input bind:value={attempts} type="number" />
+    <br />
 
-Favorable outcomes: 
-<br>
-Total outcomes: 
-<br>
-Trials: 
-
-<br>
-
-Probability of success on every trial: {(((favorable / total)**attempts)*100).toFixed(4)}%
-
-</p>
-<p>
-<input bind:value={favorable} type="number">
-<br>
-<input bind:value={total} type="number">
-<br>
-<input bind:value={attempts} type="number">
-<br>
-
-</p>
+  </p>
 </div>
