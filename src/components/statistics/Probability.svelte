ce8fc7be75cf00e9e0c9b06c2464953f363eaf36
Brenden Ferguson <brendenferguson@gmail.com>
2020-05-20 20:11:28 -0700

    probability fix

--- src/components/statistics/Probability.svelte
+++ src/components/statistics/Probability.svelte
@@ -8,17 +8,37 @@ let attempts = 1
 
 </script>
 
-<p>
+<style>
+#main{
+    display:grid;
+    grid-template-columns: 250px 1fr;
+}   
+#main input{
+    text-align:center;
+}
+</style>
 
-Favorable outcomes: <input bind:value={favorable} type="number">
-<br>
-Total outcomes: <input bind:value={total} type="number">
-<br>
-Trials: <input bind:value={attempts} type="number">
+<div id="main">
+<p >
 
+Favorable outcomes: 
+<br>
+Total outcomes: 
+<br>
+Trials: 
 
 <br>
 
 Probability of success on every trial: {(((favorable / total)**attempts)*100).toFixed(4)}%
 
 </p>
+<p>
+<input bind:value={favorable} type="number">
+<br>
+<input bind:value={total} type="number">
+<br>
+<input bind:value={attempts} type="number">
+<br>
+
+</p>
+</div>
