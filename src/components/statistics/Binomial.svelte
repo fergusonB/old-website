cd9bca7f8fc53c0b157f4122b8db90b451b4a268
Brenden Ferguson <brendenferguson@gmail.com>
2020-05-23 20:41:29 -0700

    mass function before adding total distribution

--- src/components/statistics/Binomial.svelte
+++ src/components/statistics/Binomial.svelte
@@ -1,3 +1,57 @@
-<h2> Binomial </h2>
+<script>
+  let x = 1; // number of successes
+  let n = 1; // total number of trials
+  let p = 0.5; // probability of success on one trial
+  let q = 1 - p;
 
-bionomial dist
+  const fac = (n) => (n ? n * fac(n - 1) : 1);
+
+  const probabilityMassFunction = (x, n, p) => {
+    q = 1 - p;
+    return (fac(n) / (fac(x) * fac(n - x))) * p ** x * q ** (n - x);
+  };
+
+  let results = probabilityMassFunction(Number(x), Number(n), Number(p));
+</script>
+
+<style>
+  .box {
+    display: flex;
+  }
+  .box input{
+      text-align:center;
+  }
+</style>
+
+<h2>Binomial</h2>
+<div class="box">
+  <p>
+    Number of successes, x :
+    <br />
+    Number of trials, n :
+    <br />
+    Probability of success on a single trial, p :
+  </p>
+
+  <p>
+    <input bind:value={x} type="number" />
+    <br />
+    <input bind:value={n} type="number" />
+    <br />
+    <input bind:value={p} type="number" />
+    <br />
+  </p>
+</div>
+<button
+  on:click={() => (results = probabilityMassFunction(Number(x), Number(n), Number(p)))}>
+  Calculate
+</button>
+<br>
+<h3>Results</h3>
+<br>
+Less than {x}: 
+<br>
+Exactly {x} : {(results * 100).toFixed(4)}%
+<br>
+Greater than {x}: 
+
