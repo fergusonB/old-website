cbb4a6011f2ad1e5112ac1fbbc29a4d68dee9567
Wowbcf <brendenferguson@gmail.com>
2020-06-14 12:30:26 -0700

    emerald fixe

--- src/components/dungeons/Emeralds.svelte
+++ src/components/dungeons/Emeralds.svelte
@@ -6,37 +6,40 @@ export let display
 
 let emeralds = 0
 
-$: if (process.browser && valid){
+
+
+
+$:if (process.browser && valid){
     emeralds = display.currency[0].count
 }
 else{
     emeralds = 0
 }
 
-const modEmeralds = ()=>{
+const modEmeralds = (emer)=>{
     if (process.browser && valid){
         let temp = localStorage.dungeons
         temp = temp.split('\n')
         temp[0] = JSON.parse(temp[0])
-        temp[0].currency[0].count = emeralds
+        temp[0].currency[0].count = emer
         temp[0] = JSON.stringify(temp[0])
         temp = temp.join('\n')
         localStorage.dungeons = temp
 
-
-        
     }
     else{
         alert('Please enter valid save data first')
     }
 }
 
+$:emer = emeralds
 
 </script>
 
 <h2>Emeralds</h2>
 
 
-<input bind:value={emeralds} type="number">
-<button on:click={modEmeralds}>Edit</button>
+<br>
+<input bind:value={emer} type="number">
+<button on:click={modEmeralds(emer)}>Edit</button>
 
