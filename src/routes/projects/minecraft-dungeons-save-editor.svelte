4f073e2adc0107ef28ea9a170a82e39ee044706d
Wowbcf <brendenferguson@gmail.com>
2020-06-14 10:30:31 -0700

    data bind

---
+++ src/routes/projects/minecraft-dungeons-save-editor.svelte
@@ -0,0 +1,38 @@
+<script>
+import Emeralds from "../../components/dungeons/Emeralds.svelte";
+
+let data = []
+
+if (process.browser && localStorage.dungeons){
+    data = localStorage.dungeons
+}
+
+$: if (process.browser){
+    localStorage.dungeons=data
+}
+
+
+</script>
+
+<svelte:head>
+  <title>MineCraft Dungeons Save Editor</title>
+</svelte:head>
+
+<h1>MineCraft Dungeons Save Editor</h1>
+
+<p>
+This editor was designed for the Switch version of the game.
+<br>
+It's likely that it will also work on console versions of the game. It will not work for the PC version.
+</p>
+
+<p>
+To begin, paste the <strong>contents</strong> of a character file below (e.g. Character174B9BB908D803BDD4E3EE64BB9A4F84)
+<br>
+<input type="text" bind:value={data}>
+
+</p>
+
+
+<Emeralds/>
+
