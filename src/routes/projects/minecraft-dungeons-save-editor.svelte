54e771d0868e35ec287a05a97a413a2bc99a3cdf
Wowbcf <brendenferguson@gmail.com>
2020-06-14 13:47:02 -0700

    more changes to desc

--- src/routes/projects/minecraft-dungeons-save-editor.svelte
+++ src/routes/projects/minecraft-dungeons-save-editor.svelte
@@ -38,11 +38,10 @@ $: if (process.browser){
 </svelte:head>
 
 <h1>MineCraft Dungeons Save Editor</h1>
-
 <p>
 This editor was designed for the Switch version of the game.
 <br>
-It's likely that it will also work on console versions of the game. It will not work for the PC version.
+It's likely that it will also work on other console versions of the game. It will not work for the PC version.
 <br>
 Do not bring any items you make here online, I haven't integrated any kind of legality checking. This is for testing / Min-Maxing only.
 </p>
@@ -50,13 +49,14 @@ Do not bring any items you make here online, I haven't integrated any kind of le
 <p>
 To begin, paste the <strong>contents</strong> of a character file below (e.g. Character174B9BB908D803BDD4E3EE64BB9A4F84)
 <br>
-To get this file from the Switch, you need to have a hacked console, and a data tool like JKSV. There are plenty of resources online so I will not go into them here.
+To get this file from the Switch, you need to have a hacked console, and a data tool like JKSV. 
+There are plenty of resources online so I will not go into them here.
 <br>
 
 
 <textarea style="width:50%;height:5em;" bind:value={data}/>
 <br>
-The edits will update this text area. You can simply paste it back into the save file once you have made your changes.
+The edits will update this text area. You can simply paste it back over the save file contents once you have made your changes.
 <br>
 
 <strong>Make sure you have a backup before making changes.</strong>
