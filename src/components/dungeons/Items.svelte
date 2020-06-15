e79625930487339c4f149b2dfd2ad273dffe9c97
Wowbcf <brendenferguson@gmail.com>
2020-06-14 17:53:08 -0700

    itemlist

--- src/components/dungeons/Items.svelte
+++ src/components/dungeons/Items.svelte
@@ -3,36 +3,104 @@
 export let display = []
 export let valid
 export let data
-let col='white'
 let powerSet = []
 
-const save = (i) => {
+const save = (i, ip) => {
     if (process.browser && valid){
         let temp = localStorage.dungeons
         temp = temp.split('\n')
         temp[0] = JSON.parse(temp[0])
 
 
-        temp[0].items[i].power = 5
+        temp[0].items[i].power = ip
 
         
         temp[0] = JSON.stringify(temp[0])
         temp = temp.join('\n')
         localStorage.dungeons = temp
         data = localStorage.dungeons
-        col = '#17dd62'
-        setTimeout(() => {  col='white'; }, 500);
+        alert('Saved!')
     }
     else{
         alert('Error, please refresh the page.')
     }
 }
 
+let enchantList = [
+  'Thundering',
+  'Echo',
+  'Prospector',
+  'Swirling',
+  'Freezing',
+  'RadianceMelee',
+  'FireTrail',
+  'HealthSynergy',
+  'Snowing',
+  'Celerity',
+  'GravityPulse',
+  'FinalShout',
+  'Accelerating',
+  'RadianceRanged',
+  'WildRage',
+  'Punch',
+  'ChainReaction',
+  'Gravity',
+  'Unset',
+  'Rampaging',
+  'FireAspect',
+  'Weakening',
+  'Smiting',
+  'SpeedSynergy',
+  'Chilling',
+  'Unchanting',
+  'PoisonedRanged',
+  'FuseShot',
+  'Recycler',
+  'Thorns',
+  'Swiftfooted',
+  'Protection',
+  'Frenzied',
+  'SpiritSpeed',
+  'PotionFortification',
+  'Supercharge',
+  'RapidFire',
+  'TempoTheft',
+  'Cowardice',
+  'Chains',
+  'Shockwave',
+  'Burning',
+  'Stunning',
+  'GravityMelee',
+  'Deflecting',
+  'BonusShot',
+  'Power',
+  'MultiShot',
+  'Explorer',
+  'Leeching',
+  'Exploding',
+  'Looting',
+  'Piercing',
+  'CriticalHit',
+  'Infinity',
+  'Growing',
+  'Committed',
+  'PoisonedMelee',
+  'Sharpness',
+  'Ricochet',
+  'AnimaConduitRanged',
+  'AnimaConduitMelee',
+  'FoodReserves',
+  'SurpriseGift',
+  'Electrified',
+  'SoulSiphon'
+]
+
 </script>
 
 <h2>Items</h2>
 
 <style>
+
 .container{
     display:grid;
     grid-template-columns: 1fr 1fr 1fr;
@@ -47,15 +115,18 @@ const save = (i) => {
 {#if process.browser && typeof(display) === "object" && display.items.length > 0}
     <div class="container">
         {#each display.items as item,i}
-        <div class="card" style={`background-color:${item.rarity === 'Unique' ? '#ff7500': item.rarity === 'Rare' ? '#10a66c' : 'white'};`}>
+        <div class="card" style={`background-color:${item.rarity === 'Unique' ? '#ff7500': item.rarity === 'Rare' ? '#10a66c' : 'white'}`}>
             <h3>{item.equipmentSlot || `Item Slot ${i-5}`}</h3>
             <br>
-            <strong>{item.type}</strong>
+            <strong >{item.type}</strong>
             <br>
             {item.rarity}
             <br>
             Approx. Power: {Math.round(9.98633 * item.power - 9.39419)}
-    
+            <br>
+            <input bind:value={item.power} min={1.445555555555555} max={11.755555555555555}  step={0.000000000000001} type="range"/>
+            <br>
+             {item.power}
             <br>
 
             <br> 
@@ -67,9 +138,11 @@ const save = (i) => {
                     {/if}
                 {/each}
 
+                
+
             {/if}
 
-            <button on:click|preventDefault={()=>save(i)} style="float:right;">Save</button>
+            <button on:click|preventDefault={()=>save(i,item.power)} style="float:right;">Save</button>
         </div>
         {/each}
     </div>
