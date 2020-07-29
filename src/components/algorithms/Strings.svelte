7cecde0429fcd2b9325d7c06455f46b8365a4867
unknown <brendenferguson@gmail.com>
2020-07-29 10:22:12 -0700

    algo start

---
+++ src/components/algorithms/Strings.svelte
@@ -0,0 +1,53 @@
+<script>
+    $: showHide = {
+        palindrome: false,
+    };
+
+    String.prototype.palindrome = function () {
+        let str = this.toLowerCase();
+        let left = 0;
+        let right = str.length - 1;
+        while (left < right) {
+            if (str[left] !== str[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+    let palindromeString = "Repaper";
+</script>
+
+<p>All of these extend the built in String object.</p>
+
+<h3>Check for Palindrome</h3>
+<p>
+    Checks if a string is a palindrome.
+    <br />
+    <input
+        on:click={() => (palindromeString = '')}
+        bind:value={palindromeString}
+        type="text" />
+    {palindromeString.palindrome() ? `${palindromeString} is a palindrome.` : `${palindromeString} is not palindrome.`}
+    <br />
+    <button on:click={() => (showHide.palindrome = !showHide.palindrome)}>
+        Show / Hide Code
+    </button>
+    <br />
+    {#if showHide.palindrome}
+        Usage:
+        <code>String.palindrome()</code>
+        <br />
+        Time: O(n)
+        <br />
+        Space: O(1)
+        <br />
+        <pre>{String.prototype.palindrome}</pre>
+        <br />
+    {/if}
+</p>
+
+<h3>Caesar Cipher</h3>
+<p />
---
+++ src/routes/projects/algorithms.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+  <title>Algorithms Library</title>
+</svelte:head>
+
+<h1>Common Algorithms</h1>
+
+<script>
+import Strings from "../../components/algorithms/Strings.svelte";
+
+
+let show = {
+    strings: false
+}
+
+</script>
+
+<h2>Strings <button on:click={()=>show.strings = !show.strings}>show / hide</button></h2>
+{#if show.strings}
+    <Strings />
+{/if}
