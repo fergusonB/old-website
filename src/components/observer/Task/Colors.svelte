988632d6ee890b6ed6d5be62437158d708d4dcc3
unknown <brendenferguson@gmail.com>
2020-09-01 15:23:56 -0700

    formatting

--- src/components/observer/Task/Colors.svelte
+++ src/components/observer/Task/Colors.svelte
@@ -1,127 +1,63 @@
 <script lang="typescript">
     import { plates } from "../stores.js";
-export let plateNumber
-export let noteNumber
+    export let plateNumber;
+    export let noteNumber;
 
+    let show = false;
 
-let show = false;
+    // 0:normal, 1:light, 2:dark
+    const colorData = {
+        white: ["#FFFFFF", "#ddd", "#000000"],
+        red: ["#d50000", "#ff5131", "#9b0000"],
+        pink: ["#c51162", "#fd558f", "#8e0038"],
+        purple: ["#aa00ff", "#e254ff", "#7200ca"],
+        deepPurple: ["#6200ea", "#9d46ff", "#0a00b6"],
+        indigo: ["#304ffe", "#7a7cff", "#0026ca"],
+        blue: ["#2962ff", "#768fff", "#0039cb"],
+        lightBlue: ["#0091ea", "#64c1ff", "#0064b7"],
+        cyan: ["#00b8d4", "#62ebff", "#0088a3"],
+        teal: ["#00bfa5", "#5df2d6", "#008e76"],
+        green: ["#00c853", "#5efc82", "#009624"],
+        lightGreen: ["#64dd17", "#9cff57", "#1faa00"],
+        lime: ["#aeea00", "#e4ff54", "#79b700"],
+        yellow: ["#ffd600", "#ffff52", "#c7a500"],
+        amber: ["#ffab00", "#ffdd4b", "#c67c00"],
+        orange: ["#ff6d00", "#ff9e40", "#c43c00"],
+        deepOrange: ["#dd2c00", "#ff6434", "#a30000"],
+    };
+    //
 
-// 0:normal, 1:light, 2:dark
-const colorData = {
-  "red": [
-    "#d50000",
-    "#ff5131",
-    "#9b0000"
-  ],
-  "pink": [
-    "#c51162",
-    "#fd558f",
-    "#8e0038"
-  ],
-  "purple": [
-    "#aa00ff",
-    "#e254ff",
-    "#7200ca"
-  ],
-  "deepPurple": [
-    "#6200ea",
-    "#9d46ff",
-    "#0a00b6"
-  ],
-  "indigo": [
-    "#304ffe",
-    "#7a7cff",
-    "#0026ca"
-  ],
-  "blue": [
-    "#2962ff",
-    "#768fff",
-    "#0039cb"
-  ],
-  "lightBlue": [
-    "#0091ea",
-    "#64c1ff",
-    "#0064b7"
-  ],
-  "cyan": [
-    "#00b8d4",
-    "#62ebff",
-    "#0088a3"
-  ],
-  "teal": [
-    "#00bfa5",
-    "#5df2d6",
-    "#008e76"
-  ],
-  "green": [
-    "#00c853",
-    "#5efc82",
-    "#009624"
-  ],
-  "lightGreen": [
-    "#64dd17",
-    "#9cff57",
-    "#1faa00"
-  ],
-  "lime": [
-    "#aeea00",
-    "#e4ff54",
-    "#79b700"
-  ],
-  "yellow": [
-    "#ffd600",
-    "#ffff52",
-    "#c7a500"
-  ],
-  "amber": [
-    "#ffab00",
-    "#ffdd4b",
-    "#c67c00"
-  ],
-  "orange": [
-    "#ff6d00",
-    "#ff9e40",
-    "#c43c00"
-  ],
-  "deepOrange": [
-    "#dd2c00",
-    "#ff6434",
-    "#a30000"
-  ]
-}
-//
-
-const colors = []
-for (let each in colorData){
-    colors.push(each)
-}
-
-
+    const colors = [];
+    for (let each in colorData) {
+        colors.push(each);
+    }
 </script>
 
 <style>
-    .colors{
-        display:grid;
-        grid-template-columns: repeat(3,1fr);
+    .colors {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
         justify-content: space-between;
-
     }
-    .colorBlock{
-        margin:0.5em;
+    .colorBlock {
+        margin: 0.5em;
         height: 2em;
-        width:2em;
+        width: 2em;
+        outline: 1px solid black;
     }
 </style>
 
-
-    <button on:click|self={()=>show = !show}>Select Color</button>
-    {#if show}
+<button on:click|self={() => (show = !show)}>Select Color</button>
+{#if show}
     <div class="colors">
         {#each colors as color}
-            <div on:click|self={()=>{
-                $plates[plateNumber].notes[noteNumber].colors = colorData[color]
-                show = !show
-            }} style={`background-color:${colorData[color][0]}`} class="colorBlock"/>
+            <div
+                on:click|self={() => {
+                    $plates[plateNumber].notes[noteNumber].colors = colorData[color];
+                    show = !show;
+                }}
+                style={`background-color:${colorData[color][0]}`}
+                class="colorBlock" />
         {/each}
-        </div>
-    {/if}
+    </div>
+{/if}
--- src/components/observer/Task/Summary.svelte
+++ src/components/observer/Task/Summary.svelte
@@ -1,39 +1,44 @@
 <script lang="typescript">
     import { plates } from "../stores.js";
-    export let plateNumber
+    export let plateNumber;
     export let noteNumber;
-    import Colors from './Colors.svelte';
+    import Colors from "./Colors.svelte";
 
+    $: backgroundColors = $plates[plateNumber].notes[noteNumber].colors || [
+        "white",
+        "white",
+        "black",
+    ];
 
-   $:backgroundColors = $plates[plateNumber].notes[noteNumber].colors || ['white','white','black']
-
-    let title = ''
+    let title = "";
 
     const summ = {
-        text : '',
+        text: "",
 
         save: () => {
             $plates = $plates;
         },
         exit: () => {
-            $plates[plateNumber].notes[noteNumber].clicked = !$plates[plateNumber].notes[noteNumber].clicked;
+            $plates[plateNumber].notes[noteNumber].clicked = !$plates[
+                plateNumber
+            ].notes[noteNumber].clicked;
             summ.save();
         },
-        updateSummary: ()=>{
-            $plates[plateNumber].notes[noteNumber].summary = summ.text
-            summ.save()
+        updateSummary: () => {
+            $plates[plateNumber].notes[noteNumber].summary = summ.text;
+            summ.save();
         },
-        updateTitle: ()=>{
-            $plates[plateNumber].notes[noteNumber].text = title
-            summ.save()
-        }
+        updateTitle: () => {
+            $plates[plateNumber].notes[noteNumber].text = title;
+            summ.save();
+        },
     };
 
-    if ($plates[plateNumber].notes[noteNumber].summary){
-        summ.text = $plates[plateNumber].notes[noteNumber].summary
+    if ($plates[plateNumber].notes[noteNumber].summary) {
+        summ.text = $plates[plateNumber].notes[noteNumber].summary;
     }
-    if ($plates[plateNumber].notes[noteNumber].text){
-        title = $plates[plateNumber].notes[noteNumber].text
+    if ($plates[plateNumber].notes[noteNumber].text) {
+        title = $plates[plateNumber].notes[noteNumber].text;
     }
 </script>
 
@@ -48,41 +53,50 @@
         z-index: 1;
         align-items: center;
         justify-content: center;
-        opacity:.9;
+        opacity: 0.9;
     }
     .summaryPanel {
         display: block;
         width: 80%;
         max-width: 500px;
         z-index: 2;
-
     }
 
-    textarea{
-        width:100%;
+    textarea {
+        width: 100%;
         max-width: 500px;
-        min-width:100%;
-        min-height:20em;
-
+        min-width: 100%;
+        min-height: 20em;
     }
-
 </style>
 
-<div class="summary" on:click|self={()=>summ.exit()} style={`background-color:${backgroundColors[1]};`}>
-    <div style={`background-color:${backgroundColors[0]};`}  class="summaryPanel card">
+<div
+    class="summary"
+    on:click|self={() => summ.exit()}
+    style={`background-color:${backgroundColors[1]};`}>
+    <div
+        style={`background-color:${backgroundColors[0]};`}
+        class="summaryPanel card">
         <button style="float:right;" on:click={() => summ.exit()}>❌</button>
-        <h3>Notes:</h3><input  style={`color:${backgroundColors[2]};`} bind:value={title} on:blur={()=>summ.updateTitle()} type="text">
+        <h3>Notes:</h3><input
+            style={`color:${backgroundColors[2]};`}
+            bind:value={title}
+            on:blur={() => summ.updateTitle()}
+            type="text" />
 
         <h4>Summary:</h4>
-        
-        <p>
-            <textarea style={`color:${backgroundColors[2]};`} bind:value={summ.text} rows="10" name="summary" on:blur={()=>summ.updateSummary()}/>
 
+        <p>
+            <textarea
+                style={`color:${backgroundColors[2]};`}
+                bind:value={summ.text}
+                rows="10"
+                name="summary"
+                on:blur={() => summ.updateSummary()} />
         </p>
         <p>
-        <Colors {plateNumber} {noteNumber}/>
-    </p>
-
+            <Colors {plateNumber} {noteNumber} />
+        </p>
     </div>
 </div>
 
