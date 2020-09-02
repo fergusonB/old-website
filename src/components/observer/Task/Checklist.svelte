d3a44d0a0e1c85c46b4675e30d98e54a2d44af0c
unknown <brendenferguson@gmail.com>
2020-09-01 17:39:59 -0700

    checklist progress

--- src/components/observer/Task/Checklist.svelte
+++ src/components/observer/Task/Checklist.svelte
@@ -1,10 +1,12 @@
 <script lang="typescript">
+        import { plates } from "../stores.js";
+
     let inputField = "new checkbox";
 
     const checklist = {
         exists: false,
 
-        list: [{ name: "hello", checked: false }],
+        list: [],
 
         add: () => {
             checklist.list = [
@@ -12,6 +14,21 @@
                 { name: inputField, checked: false },
             ];
         },
+
+        progress:()=>{
+            let count = 0
+            for (let item of checklist.list){
+                if (item.checked) count++
+            }
+            return (count / checklist.list.length) || 0
+        },
+
+        delete:()=>{
+            checklist.list = []
+            checklist.exists = false
+        }
+
+
     };
 </script>
 
@@ -23,7 +40,12 @@
 </style>
 
 {#if checklist.exists}
+<br>
+<progress value={checklist.progress()}></progress> <span on:click|self={()=>checklist.delete()}>🗑️</span>
+
+
     <p>
+        
         {#each checklist.list as item, i}
             <span
                 on:click={() => (item.checked = !item.checked)}
