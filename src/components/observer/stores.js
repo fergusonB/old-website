731afbc85c3cc118430fe5f0621e7bd39f5d7363
unknown <brendenferguson@gmail.com>
2020-09-02 20:10:13 -0700

    new instructions

--- src/components/observer/stores.js
+++ src/components/observer/stores.js
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export let plates = writable([{ title: "To Do", notes:[{clicked:false,text:'Click me',summary:'Now try making a new plate'}] }]);
+export let plates = writable([{"title":"To Do","notes":[{"text":"Click me","clicked":false,"summary":"Look at the checklist below","checklist":[{"name":"click me","checked":false},{"name":"select a color","checked":false},{"name":"make a new plate","checked":false},{"name":"make some new tasks","checked":false},{"name":"drag a plate around","checked":false},{"name":"drag some tasks around","checked":false}]}]}]);
 
 export let note1 = writable([]);
 export let note2 = writable([]);
