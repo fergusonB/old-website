4c1640c385545bd455cd7e93aaad66013d265d6b
unknown <brendenferguson@gmail.com>
2020-09-18 09:31:03 -0700

    fixed 0,0 error

--- src/components/observer/Task/Plate.svelte
+++ src/components/observer/Task/Plate.svelte
@@ -1,9 +1,8 @@
 <script lang="typescript">
     export let plateNumber;
 
-
-    import { plates,note1,note2, dragTask } from "../stores.js";
-    import {afterUpdate} from 'svelte';
+    import { plates, note1, note2, dragTask } from "../stores.js";
+    import { afterUpdate } from "svelte";
     import Summary from "./Summary.svelte";
 
     if ($plates[plateNumber].notes === undefined) {
@@ -31,22 +30,19 @@
         },
     };
 
+    const swapNotes = () => {
+        if (
+            $plates[$note1[0]].notes[$note1[1]] !== undefined &&
+            $plates[$note2[0]].notes[$note2[1]] !== undefined
+        ) {
+            let tmp = $plates[$note1[0]].notes[$note1[1]];
+            $plates[$note1[0]].notes[$note1[1]] =
+                $plates[$note2[0]].notes[$note2[1]];
+            $plates[$note2[0]].notes[$note2[1]] = tmp;
 
-    const swapNotes= ()=>{
-
-
-
-        let tmp = $plates[$note1[0]].notes[$note1[1]]
-        $plates[$note1[0]].notes[$note1[1]] = $plates[$note2[0]].notes[$note2[1]]
-        $plates[$note2[0]].notes[$note2[1]] = tmp
-        
-        setTimeout(()=>$dragTask = true,0) 
-    }
-
-
-
-
-
+            setTimeout(() => ($dragTask = true), 0);
+        }
+    };
 </script>
 
 <style>
@@ -71,29 +67,34 @@
         cursor: pointer;
         background-color: #ddd;
     }
-    .content-edit-text{
-        cursor:text;
+    .content-edit-text {
+        cursor: text;
     }
 </style>
 
-<h3> <span class="content-edit-text" contenteditable="true" bind:innerHTML={$plates[plateNumber].title} >{$plates[plateNumber].title}</span> </h3>
+<h3>
+    <span
+        class="content-edit-text"
+        contenteditable="true"
+        bind:innerHTML={$plates[plateNumber].title}>{$plates[plateNumber].title}</span>
+</h3>
 
 {#each $plates[plateNumber].notes as note, noteNumber}
     <div
-    on:dragover|preventDefault|self
-    on:dragend|preventDefault|self
-    draggable={true} on:dragstart={()=>{
-        $dragTask = false
-        $note1=[plateNumber,noteNumber]
-    
-    }}
-     on:dragenter={()=>$note2=[plateNumber,noteNumber]}
-      on:dragend={()=>swapNotes()}
+        on:dragover|preventDefault|self
+        on:dragend|preventDefault|self
+        draggable={true}
+        on:dragstart={() => {
+            $dragTask = false;
+            $note1 = [plateNumber, noteNumber];
+        }}
+        on:dragenter={() => ($note2 = [plateNumber, noteNumber])}
+        on:dragend={() => swapNotes()}
         style={`background-color:${note.colors ? note.colors[1] : 'white'};`}
         class="note card"
         on:click|self={() => {
             note.clicked = !note.clicked;
-            $dragTask = false
+            $dragTask = false;
         }}>
         {note.text}
         <span
@@ -113,28 +114,23 @@
     </div>
 
     {#if note.clicked}
-        <Summary {plateNumber} {noteNumber}/>
+        <Summary {plateNumber} {noteNumber} />
     {/if}
 {/each}
 
-
 <input
-placeholder='Add a new note'
+    placeholder="Add a new note"
     on:dragover|preventDefault|self
     on:dragend|preventDefault|self
-    
-    on:drop={()=>{
-        if (!(plateNumber === $note1[0])){
-            $plates[plateNumber].notes = [...$plates[plateNumber].notes,$plates[$note1[0]].notes[$note1[1]]]
-            $plates[$note1[0]].notes = [...$plates[$note1[0]].notes.filter(x=>x!==$plates[$note1[0]].notes[$note1[1]])]
-            setTimeout(()=>$dragTask = true,0) 
+    on:drop={() => {
+        if (!(plateNumber === $note1[0])) {
+            $plates[plateNumber].notes = [...$plates[plateNumber].notes, $plates[$note1[0]].notes[$note1[1]]];
+            $plates[$note1[0]].notes = [...$plates[$note1[0]].notes.filter((x) => x !== $plates[$note1[0]].notes[$note1[1]])];
+            setTimeout(() => ($dragTask = true), 0);
         }
-
     }}
-
     on:click={() => (newNote.text = '')}
     on:blur={() => newNote.create()}
     on:keydown={(e) => newNote.enterKey(e)}
     bind:value={newNote.text}
     type="text" />
-
