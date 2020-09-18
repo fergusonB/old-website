46b6311a72bd4bca9374e63cd4c4233efee0b617
unknown <brendenferguson@gmail.com>
2020-09-18 12:07:53 -0700

    footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,67 +1,16 @@
 <style>
-    h4{
-        padding:10px;
-
-    }
     footer{
-
-
-        
-
-        
-        color:white;
-        display:flex;
-
-        background-color: black;
-        position: absolute;
-        width:100%;
-        bottom:0;
-
-        list-style-type:none;
-        box-shadow: 1px -2px 1px 1px rgba(51,51,51,.2);
-
-        justify-content: center;
-
-
-
-
+        margin-bottom: 40px auto 0;
+        text-align: center;
+        color:#bbb;
+        padding:20px;
+        max-width: 400px;
+        border-top:1px solid #ddd;
+        margin-left:auto;
+        margin-right:auto;
     }
-
-    
-
-li{
-    list-style-type: none;
-}
-
-
 </style>
-<div style="padding:2em;"></div>
 
 <footer>
-
-    <nav  aria-labelledby="footer-links" >
-        <div>
-            <h4><strong>Navigation</strong></h4>
-        <ul>
-          <li><a href="https://ferguson.work/about">About</a></li>
-          <li><a href="https://ferguson.work/blog">Blog</a></li>
-          <li><a href="https://ferguson.work/projects">Projects</a></li>
-
-        </ul>
-    </div>
-</nav>
-
-    <nav aria-labelledby="footer-social-media" >
-        <h4><strong>Social Media</strong></h4>
-    <div>
-        <ul>
-          <li><a href="https://github.com/fergusonB">GitHub</a></li>
-          <li><a href="https://www.linkedin.com/in/brendenferguson/">LinkedIn</a></li>
-
-        </ul>
-    </div>
-</nav>
-
-
-
+Copyright © 2019-2020 Brenden Ferguson
 </footer>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -33,4 +33,8 @@
 
 </main>
 
+
+
+<Footer />
+
 </div>
