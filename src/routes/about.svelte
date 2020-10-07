b85b55e66a6da01e37bf53bc7940d6a556703b1f
unknown <brendenferguson@gmail.com>
2020-10-06 17:14:30 -0700

    shortened the about page, still needs some work

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,4 @@
-<style>
-  img {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    max-width: 100%;
-    height: auto;
-    filter: drop-shadow(4px 4px 4px #121212);
-    border-radius: 4px;
-  }
-</style>
+
 
 <svelte:head>
   <title>About</title>
@@ -26,22 +16,16 @@ identity is no way tied to these but it should give some more color than a stand
   I've always loved the internet and building things.
   <br>
   I'm currently interested in building web-based programs, scraping, statistics, and machine learning.
-  <br>
-  I also enjoy linux-based servers; my home automation is run on a raspberry Pi, and I have a weird obsession dirt-cheap VPS' and collect them as others would collect stamps or bottlecaps.
+
 </p>
 
 <p>I have a BBA in Entrepreneurial and Small Business Operations from CSUF, and was an account manager for the largest plumbing wholesaler in the nation for three years. While there I developed tools to improve sales efficiency, including logistics tools and sales calculators.</p>
 
-<h2>About This Site</h2>
+
 <p>
-  This site is purely HTML, CSS, and Javascript exported from Sapper, hosted on GitHub Pages with SSL and Workers from Cloudflare.
+  MBTI: INTJ
   <br>
-    <br />
-  I write everything on this site in Svelte, and VSCode is currently my editor of choice.
-
-  <br />
-  <br />
-  <img src="./assets/about/lighthouse.png" alt="lighthouse" />
-
+  DiSC : DC
 </p>
+
 </article>
