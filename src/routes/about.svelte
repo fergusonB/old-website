b5fc19acb5fb3be5b2f7c70ef8e0aec10b50bb9f
unknown <brendenferguson@gmail.com>
2020-10-14 11:07:53 -0700

    simpler about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -6,26 +6,18 @@
 <article>
 <h1>About Me</h1>
 
-<!--add information on intj  / the other dp or something, maybe something about typing - just stuff that would make me stand out a little
-identity is no way tied to these but it should give some more color than a standard interview would provide
--->
 
 <p>I'm Brenden.</p>
 
 <p>
-  I've always loved the internet and building things.
+  I'm a front-end developer based in Southern California.
   <br>
-  I'm currently interested in building web-based programs, scraping, statistics, and machine learning.
+  I have a BBA in Entrepreneurial and Small Business Operations from CSUF, and was an account manager for the largest plumbing wholesaler in the nation for three years.
+  <br> While there I discovered a passion for programming, developing tools to improve sales efficiency.
 
 </p>
 
-<p>I have a BBA in Entrepreneurial and Small Business Operations from CSUF, and was an account manager for the largest plumbing wholesaler in the nation for three years. While there I developed tools to improve sales efficiency, including logistics tools and sales calculators.</p>
 
 
-<p>
-  MBTI: INTJ
-  <br>
-  DiSC : DC
-</p>
 
 </article>
